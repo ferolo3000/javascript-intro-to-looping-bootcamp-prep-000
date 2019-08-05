e58bab602d84b7ed9aa4281f01745e88c5943779
Fernanda Romero <fernandaromero85@gmail.com>
2019-08-05 22:30:00 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -29,7 +29,7 @@ function incrementVariable() {
 }
 
 do {
-  console.log("doo-bee-doo-bee-doo");
-} while (incrementVariable() < 5);
+  console.log("I run once regardless.");
+} while (incrementVariable() < num);
 
 } 
